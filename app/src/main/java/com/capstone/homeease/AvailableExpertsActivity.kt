9d132391ce651c33497c6cc11353f01d27087fc4
jember7 <jembermanacap@gmail.com>
2024-11-08 11:33:48 +0800

    available experts and activity page

---
+++ app/src/main/java/com/capstone/homeease/AvailableExpertsActivity.kt
@@ -0,0 +1,43 @@
+package com.capstone.homeease
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView // Import ImageView
+import com.capstone.homeease.R
+
+class AvailableExpertsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_available_experts)
+
+        // Find ImageViews by their IDs
+        val activityImageView = findViewById<ImageView>(R.id.activity)
+        val paymentImageView = findViewById<ImageView>(R.id.payment)
+        val textHomeImageView = findViewById<ImageView>(R.id.textHome)
+        val messagesImageView = findViewById<ImageView>(R.id.messages)
+        val profileImageView = findViewById<ImageView>(R.id.profile)
+
+        // Set click listeners for ImageViews
+        activityImageView.setOnClickListener {
+            val intent = Intent(this, ActivityPage::class.java)
+            startActivity(intent)
+        }
+        paymentImageView.setOnClickListener {
+            val intent = Intent(this, PaymentPageActivity::class.java)
+            startActivity(intent)
+        }
+        textHomeImageView.setOnClickListener {
+            val intent = Intent(this, UserDashBoard::class.java)
+            startActivity(intent)
+        }
+        messagesImageView.setOnClickListener {
+            val intent = Intent(this, MessagesPageActivity::class.java)
+            startActivity(intent)
+        }
+        profileImageView.setOnClickListener {
+            val intent = Intent(this, ProfilePageActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
